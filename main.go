95d313ce186122fb8b6e412df6a17659ad09328a
Nathan Kjer <home@Nathans-iMac.local>
2023-04-18 01:48:26 -0400

    Update to fix CORS issue

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	router := gin.Default()
 
 	config := cors.DefaultConfig()
-	config.AllowAllOrigins = true
+	config.AllowOrigins = []string{"https://tickertwins.com"}
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"}
 	router.Use(cors.New(config))
