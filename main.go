033ea4d29387e3e34e1e0f1c7be824c3535c4585
Nathan Kjer <nathan.k@hey.com>
2023-04-18 13:04:23 -0400

    Update main.go

--- main.go
+++ main.go
@@ -13,12 +13,6 @@ import (
 func main() {
 	router := gin.Default()
 
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"https://tickertwins.com", "https://tickertwins-frontend-vct4oolaqq-ue.a.run.app/"}
-	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
-	config.AllowHeaders = []string{"Origin", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"}
-	router.Use(cors.New(config))
-
 	router.GET("/tickers", controllers.GetTickers)
 	router.GET("/tickers/:symbol/similar", controllers.GetSimilarTickers)
 
