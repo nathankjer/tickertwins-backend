f9dd68b53aba8a1e8e3e3073aaeab126d740f457
Nathan Kjer <home@Nathans-iMac.local>
2023-04-18 08:05:34 -0400

    Add cloud run instance to origins

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	router := gin.Default()
 
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"https://tickertwins.com"}
+	config.AllowOrigins = []string{"https://tickertwins.com", "https://tickertwins-frontend-vct4oolaqq-ue.a.run.app/"}
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"}
 	router.Use(cors.New(config))
